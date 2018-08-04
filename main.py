6a67862a4810c6532ab821be7a18c353b2b335ea
jeub <jurgen.palsma@gmail.com>
2018-08-04 18:04:15 +0100

    save results

--- main.py
+++ main.py
@@ -38,7 +38,30 @@ def run_multiple_pso(path, datafile, ff, cfg):
     for e in experiments:
         e.join()
 
-    pass
+def run_multiple_csfla(path, datafile, ff, cfg):
+    """
+        Runs multiple csfla experiments when -f flag is passed.
+        Takes a path to list of configurations (json file) and executes each provided config on a different process
+    """
+    logger = logging.getLogger(__name__)
+    if not os.path.exists(path):
+        logger.error("%s: csfla config file not found" % path)
+        quit()
+    with open(path) as cfg_file:  
+        csflacfg = json.load(cfg_file)
+
+    experiments = []
+
+    for p in csflacfg:
+        print('CSFLA: Running configuration: %s, results in: %s' % (p, csflacfg[p]['base_results_file_path']))
+        logger.info('CSFLA: Running configuration: %s, results in: %s' % (p, csflacfg[p]['base_results_file_path']))
+        csflacfg[p]['results_file_path'] = csflacfg[p]['base_results_file_path'] + datafile + '/'
+        csfla = Process(target=run_csfla_from_config, args=(ff, cfg['algos']['n_runs'], csflacfg[p]))
+        csfla.start()
+        experiments.append(csfla)
+
+    for e in experiments:
+        e.join()
 
 if __name__== "__main__":
 
@@ -46,7 +69,12 @@ if __name__== "__main__":
 
     #-db DATABSE -u USERNAME -p PASSWORD -size 20
     argp.add_argument("-c", "--config", help="Config file path", required=True)
-    argp.add_argument("-p", "--pso", help="Path to directory of pso configurations")
+    argp.add_argument("-p", "--pso", help="Path to pso configurations file")
+    argp.add_argument("-f", "--csfla", help="Path to csfla configurations file")
+    argp.add_argument("-d", "--default", help="Run default config for all unspecified algorithms")
+    argp.add_argument("-o", "--only", help="Run only specified flagged algorithms", action='store_true')
+
+
     args = argp.parse_args()
 
     if not os.path.exists(args.config):
@@ -63,9 +91,9 @@ if __name__== "__main__":
     for f in cfg['data']['files']:
         print("Running on file: %s" % f)
     
-        # Run GA for this batch of data
-        #ga = Process(target=subprocess.call, args=((['java', '-jar', 'dc-ga.jar', f, '1000', '35', '4', '0.90', '0.10', '0.0025', '5', '200', '500000', '-1', '0.2', '3', '1', '0.01', '0', str(cfg['algos']['n_runs']), cfg['ga']['base_results_file_path'] ]),))
-        #ga.start()
+        #Run GA for this batch of data
+        ga = Process(target=subprocess.call, args=((['java', '-jar', 'dc-ga.jar', f, '1000', '35', '4', '0.90', '0.10', '0.0025', '5', '200', '500000', '-1', '0.2', '3', '1', '0.01', '0', str(cfg['algos']['n_runs']), cfg['ga']['base_results_file_path'] ]),))
+        ga.start()
         
         # Get a free socket to communicate with server
         while not socket_is_free(portgen):
@@ -75,27 +103,33 @@ if __name__== "__main__":
         serv = Process(target=subprocess.call, args=((['java', '-jar', 'dc-server.jar', f, '1000', '35', '4', '0.90', '0.10', '0.0025', '5', '200', '500000', '-1', '0.2', '3', '1', '0.01', str(portgen)]),))
         serv.start()
         # give it some time to warm up
-        time.sleep(0.5) 
+        time.sleep(1) 
         
         # Define fitness function for all algorithms
         ff = JavaFitness(port=portgen)
+        
+        if args.csfla:
+            run_multiple_csfla(args.csfla, f, ff, cfg)
+        elif not args.only:
+            #Define and run csfla
+            cfg['csfla']['results_file_path'] = cfg['csfla']['base_results_file_path'] + f + '/'
+            csfla = Process(target=run_csfla_from_config, args=(ff,cfg['algos']['n_runs'], cfg['csfla']))
+            csfla.start()
 
         if args.pso:
             run_multiple_pso(args.pso, f, ff, cfg)
-        else:
+        elif not args.only:
             # Define and run particle swarm optimization as a new process
             cfg['pso']['results_file_path'] = cfg['pso']['base_results_file_path'] + f + '/'
             pso = Process(target=run_pso_from_config, args=(ff, cfg['algos']['n_runs'], cfg['pso']))
             pso.start()
 
-        #Define and run csfla
-        #cfg['csfla']['results_file_path'] = cfg['csfla']['base_results_file_path'] + f + '/'
-        #csfla = Process(target=run_csfla_from_config, args=(ff,cfg['algos']['n_runs'], cfg['csfla']))
-        #csfla.start()
+
 
         # Wait for all algorithms to finish and kill dc server
-        #csfla.join()
-        if not args.pso:
+        if not args.pso and not args.only:
             pso.join()
+        if not args.csfla and not args.only:
+            csfla.join()
         serv.terminate()
         portgen += 1
